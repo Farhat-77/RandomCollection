ad2b17f176c962a12a34610e09c52ede0d0f8bb7
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-02-16 12:16:45 +0600

    Добавьте файлы проекта.

---
+++ RandomCollection/ArrayList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomCollection
+{
+    public class ArrayList
+    {
+        public int Count { get; private set; }
+
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            Random random = new Random();
+            Console.WriteLine("Initial list: ");
+            int average = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                list.Add(random.Next(-20, 20));
+                average += (int)list[i];
+                Console.Write(list[i] + " ");
+            }
+            average /= list.Count;
+            Console.WriteLine("\n\nAverage:\n" + average);
+            Console.WriteLine("\nResult: ");
+            foreach (int item in list)
+            {
+                if (item > average)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+            Console.ReadLine();
+        }
+
+        private void Add(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RandomCollection/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RandomCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Random rnd = new Random();
+                Console.WriteLine("{0,4}", rnd.Next(1, 40));
+            }
+            Console.ReadKey();
+        }
+    }
+   
+}
